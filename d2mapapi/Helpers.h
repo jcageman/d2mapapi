4f4e6bdf5d5790ea9496cd5074be10cc9e7846c1
drip <imsussydrip@gmail.com>
2021-10-26 12:18:27 -0400

    added some helper functions to cleanup repeated code in main.cpp, moved helper functions to their own source file.

---
+++ d2mapapi/Helpers.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+void printHelp();
+std::string extractArg(std::string arg);
--- d2mapapi/main.cpp
+++ d2mapapi/main.cpp
@@ -4,19 +4,15 @@
 
 #include "D2Map.h"
 #include "ServerHandler.h"
-
-
+#include "Helpers.h"
 
 int main( int argc, const char* argv[] ) {
     int i;
+
 	// Check command line arguments.
 	if (argc < 2)
 	{
-		std::cerr << "Usage: program.exe {diablo2dir} [optionalArguments]\n";
-		std::cerr << "\t-i or --ip\n\t\tspecify an IP address to use instead of localhost.\n\t\tex: -i 192.168.0.1 OR --ip=192.168.0.1\n";
-		std::cerr << "\t-p or --port\n\t\tspecify an port address to use instead of the default 8080.\n\t\tex: -p 8080 OR --port=8080\n";
-		std::cerr << "\t-h or --help\n\t\tprints this usage information\n";
-		std::cerr << "Example: program.exe \"C:\\Diablo II1.13c\"\nor\nExample: program.exe \"C:\\Diablo II1.13c\" -i 0.0.0.0 -p 8888";
+		printHelp();
 		return 1;
 	}
 
@@ -37,46 +33,44 @@ int main( int argc, const char* argv[] ) {
 		// handle args in this loop:
 		for(i=2; i<argc; ++i)
 		{	
-			std::regex rgx("--\\w+=(.+)");
-			std::smatch match;
 			std::string tmpArg = argv[i];
+			std::string argVal;
 
 			if (tmpArg == "-h" || tmpArg == "--help")
 			{
-				std::cout << "Usage: program.exe {diablo2dir} [optionalArguments]\n";
-				std::cout << "\t -i or --ip\n\t\tspecify an IP address to use instead of localhost.\n\t\tex: -i 192.168.0.1 OR --ip=192.168.0.1\n";
-				std::cout << "\t-p or --port\n\t\tspecify an port address to use instead of the default 8080.\n\t\tex: -p 8080 OR --port=8080";
+				printHelp();
 				return 0;
 			}
+
 			if (tmpArg == "-i") 
-			{
 				address = argv[i + 1];
-			}
+
 			if (tmpArg == "-p")
 			{
-				char *end;
-				errno = 0;
-				long val = strtol(argv[i + 1], &end, 10);
-				if (errno || end == argv[i + 1] || *end != '\0' || val < 0 || val >= 0x10000) {
-					std::cerr << "Failed to convert port argument to uint16 from string, ensure the value passed is a valid integer\n";
+				std::string tmpPort = argv[i + 1];
+				port = std::stoi(tmpPort);
+			}
+
+			if (tmpArg.find("--ip") != std::string::npos)
+			{
+				argVal = extractArg(tmpArg);
+				if (argVal == "") {
 					return 1;
 				}
-				port = (uint16_t)val;
-			}
-			if (tmpArg.find("--ip") != std::string::npos)
-			{	
-				if (std::regex_search(tmpArg, match, rgx)) {
-					address = match[1];
-				} else {
-					std::cerr << "Failed to find valid IP address in argument --ip, using localhost";
+				else {
+					address = argVal;
 				}
 			}
-			if (tmpArg.find("--port") != std::string::npos) 
+
+
+			if (tmpArg.find("--port") != std::string::npos)
 			{
-				if (std::regex_search(tmpArg, match, rgx)) {
-					port = std::stoi(match[1]);
-				} else {
-					std::cerr << "Failed to find valid port in argument --port, using 8080";
+				argVal = extractArg(tmpArg);
+				if (argVal == "") {
+					return 1;
+				}
+				else {
+					port = std::stoi(extractArg(tmpArg));
 				}
 			}
 		}
